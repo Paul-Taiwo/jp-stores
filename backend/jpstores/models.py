f416fefb7dd99c69c82fb4f7a7c5b57cad46c4e1
Paul Taiwo <ayopaulot@gmail.com>
2023-11-04 23:45:20 +0100

    Update models.py

--- backend/jpstores/models.py
+++ backend/jpstores/models.py
@@ -77,11 +77,6 @@ class Category(models.Model):
         return self.name
 
 
-class ProductImage(models.Model):
-    image_url = CloudinaryField("image")
-    thumbnail_url = CloudinaryField("image")
-
-
 class Product(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
@@ -93,10 +88,18 @@ class Product(models.Model):
     stock = models.PositiveIntegerField()
     brand = models.CharField(max_length=50, blank=True, null=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    images = models.ManyToManyField(ProductImage)
 
     def __str__(self):
         return f"{self.title}: {self.description}"
+
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    image_url = CloudinaryField("image")
+    thumbnail_url = CloudinaryField("thumbnail")
+
+    def __str__(self):
+        return f"Image for {self.product.title}"
 
 
 class ProductReview(models.Model):

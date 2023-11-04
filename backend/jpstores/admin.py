974be78a8b0d992cf679da454cf4f4581d12dcf3
Paul Taiwo <ayopaulot@gmail.com>
2023-11-04 17:29:13 +0100

    add models to admin site

--- backend/jpstores/admin.py
+++ backend/jpstores/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import (
+    Category,
+    Product,
+    ProductImage,
+    ProductReview,
+    PurchaseHistory,
+    User,
+)
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(ProductImage)
+admin.site.register(ProductReview)
+admin.site.register(PurchaseHistory)

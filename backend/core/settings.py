ef7ce68372442ed3b1bee1e85e92c166a5d6e978
Paul Taiwo <ayopaulot@gmail.com>
2023-11-04 17:29:00 +0100

    add more cloudinary config

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -27,6 +27,8 @@ cloudinary.config(
     cloud_name=env("CLOUDINARY_CLOUD_NAME"),
     api_key=env("CLOUDINARY_API_KEY"),
     api_secret=env("CLOUDINARY_API_SECRET"),
+    secure=True,
+    upload_preset=env("CLOUDINARY_UPLOAD_PRESET"),
 )
 
 
